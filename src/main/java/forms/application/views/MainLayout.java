d75fe75de1c7a74430524864b3268d6a40586680
artem <artempolevoj@yandex.ru>
2024-08-04 20:40:15 +0300

    add setMainLayout()

--- src/main/java/forms/application/views/MainLayout.java
+++ src/main/java/forms/application/views/MainLayout.java
@@ -1,89 +1,55 @@
 package forms.application.views;
 
-import com.vaadin.flow.component.Component;
-import com.vaadin.flow.component.applayout.AppLayout;
-import com.vaadin.flow.component.html.*;
-import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.theme.lumo.LumoUtility;
-import org.vaadin.lineawesome.LineAwesomeIcon;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.router.*;
+import com.vaadin.flow.component.html.H1;
+
 
 /**
  * The main view is a top-level placeholder for other views.
  */
-public class MainLayout extends AppLayout {
 
-    private H1 viewTitle;
+@PageTitle("MainPage")
+@Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 0)
+@Route(value = "")
+@RouteAlias(value = "")
+public class MainLayout extends VerticalLayout {
 
-    public static class MenuItemInfo extends ListItem {
-
-        private final Class<? extends Component> view;
-
-        public MenuItemInfo(String menuTitle, Component icon, Class<? extends Component> view) {
-            this.view = view;
-            RouterLink link = new RouterLink();
-            // Use Lumo classnames for various styling
-            link.addClassNames(LumoUtility.Display.FLEX, LumoUtility.Gap.XSMALL, LumoUtility.Height.MEDIUM, LumoUtility.AlignItems.CENTER, LumoUtility.Padding.Horizontal.SMALL,
-                    LumoUtility.TextColor.BODY);
-            link.setRoute(view);
-
-            Span text = new Span(menuTitle);
-            // Use Lumo classnames for various styling
-            text.addClassNames(LumoUtility.FontWeight.MEDIUM, LumoUtility.FontSize.MEDIUM, LumoUtility.Whitespace.NOWRAP);
-
-            if (icon != null) {
-                link.add(icon);
-            }
-            link.add(text);
-            add(link);
-        }
-
-        public Class<?> getView() {
-            return view;
-        }
-
-    }
 
     public MainLayout() {
-        addToNavbar(createHeaderContent());
-        setDrawerOpened(false);
+        setMainLayout();
     }
 
-    private Component createHeaderContent() {
-        Header header = new Header();
-        header.addClassNames(LumoUtility.BoxSizing.BORDER, LumoUtility.Display.FLEX, LumoUtility.FlexDirection.COLUMN, LumoUtility.Width.FULL);
+    private void setMainLayout() {
+        H1 h1 = new H1("ОСМОТР ТЕХНИКИ");
+        TextArea warning = new TextArea("ПРЕДУПРЕЖДЕНИЕ");
+        warning.setValue("Сервис предназначен для осмотра техники." +
+                "В данный момент сервис находится в разработке." +
+                "Нажимая кнопку продолжить вы соглашаетесь со всеми условиями");
+        warning.setReadOnly(true);
+        Button proceed = new Button("Продолжить");
+        proceed.addClickListener(e ->
+                proceed.getUI().ifPresent(ui ->
+                        ui.navigate("InputData")));
 
-        Div layout = new Div();
-        layout.addClassNames(LumoUtility.Display.FLEX, LumoUtility.AlignItems.CENTER, LumoUtility.Padding.Horizontal.LARGE);
 
-        H1 appName = new H1("Example Vaadin");
-        appName.addClassNames(LumoUtility.Margin.Vertical.MEDIUM, LumoUtility.Margin.End.AUTO, LumoUtility.FontSize.LARGE);
-        layout.add(appName);
+        Button exit = new Button("Выход");
+         exit.addClickListener(e->{
+             UI ui = getUI().get();
+             ui.getPage().executeJs("window.location.href='logout.html'");
+             ui.getSession().close();
+         });
 
-        Nav nav = new Nav();
-        nav.addClassNames(LumoUtility.Display.FLEX, LumoUtility.Overflow.AUTO, LumoUtility.Padding.Horizontal.MEDIUM, LumoUtility.Padding.Vertical.XSMALL);
-
-        // Wrap the links in a list; improves accessibility
-        UnorderedList list = new UnorderedList();
-        list.addClassNames(LumoUtility.Display.FLEX, LumoUtility.Gap.SMALL, LumoUtility.ListStyleType.NONE, LumoUtility.Margin.NONE, LumoUtility.Padding.NONE);
-        nav.add(list);
-
-        for (MenuItemInfo menuItem : createMenuItems()) {
-            list.add(menuItem);
-
-        }
-
-        header.add(layout, nav);
-        return header;
+        HorizontalLayout buttonLayout = new HorizontalLayout(exit, proceed);
+        add(h1);
+        add(warning);
+        add(buttonLayout);
     }
 
-    private MenuItemInfo[] createMenuItems() {
-        return new MenuItemInfo[]{ //
-              //  new MenuItemInfo("Hello", LineAwesomeIcon.GLOBE_SOLID.create(), HelloView.class), //
-
-                new MenuItemInfo("FirstLevel", LineAwesomeIcon.TH_LIST_SOLID.create(), UpperLevelView.class), //
-
-              //  new MenuItemInfo("Question", LineAwesomeIcon.PENCIL_RULER_SOLID.create(), QuestionView.class), //
-
-        };
-    }
 }
+
+
