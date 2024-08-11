352c931c9c477e49b7a1a8167b7269d6a48daed0
artem <artempolevoj@yandex.ru>
2024-08-11 19:50:27 +0300

    create TypeMachineEntityServiceImp

---
+++ src/main/java/forms/application/service/TypeMachineEntityServiceImp.java
@@ -0,0 +1,44 @@
+package forms.application.service;
+
+import forms.application.model.TypeMachineEntity;
+import forms.application.repository.TypeMachineRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TypeMachineEntityServiceImp implements ServiceEntity<TypeMachineEntity> {
+    private final TypeMachineRepository typeMachineRepository;
+
+    @Override
+    public TypeMachineEntity create(TypeMachineEntity typeMachine) {
+        return typeMachineRepository.save(typeMachine);
+    }
+
+    @Override
+    public List<TypeMachineEntity> getAll() {
+        return typeMachineRepository.findAll();
+    }
+
+    @Override
+    public TypeMachineEntity getById(Long id) {
+        return typeMachineRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public TypeMachineEntity update(TypeMachineEntity typeMachine) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+        typeMachineRepository.deleteById(id);
+    }
+    public Collection<String> getAllNames(){
+        return typeMachineRepository.findAll().stream().map(TypeMachineEntity::getTypeMachine).
+                collect(java.util.stream.Collectors.toList());
+    }
+}
