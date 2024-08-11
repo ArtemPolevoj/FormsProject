ddc5ec654587b3a30249e188e17b3dc148ba4bdb
artem <artempolevoj@yandex.ru>
2024-08-11 19:51:03 +0300

    create TypeMachineRepository

---
+++ src/main/java/forms/application/repository/TypeMachineRepository.java
@@ -0,0 +1,8 @@
+package forms.application.repository;
+
+import forms.application.model.TypeMachineEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeMachineRepository extends JpaRepository<TypeMachineEntity, Long> {
+
+}
