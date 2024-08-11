3905d926eb21df1dc829f176cde76c0c032b221f
artem <artempolevoj@yandex.ru>
2024-08-11 19:44:22 +0300

    update DivisionEntityRepository

--- src/main/java/forms/application/repository/DivisionEntityRepository.java
+++ src/main/java/forms/application/repository/DivisionEntityRepository.java
@@ -3,9 +3,6 @@ package forms.application.repository;
 import forms.application.model.DivisionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface DivisionEntityRepository extends JpaRepository<DivisionEntity, Long> {
-
 }
 
