b4d9a44d90ca38f2c131fa5c43574fa2c383c914
artem <artempolevoj@yandex.ru>
2024-09-29 19:38:20 +0300

    add get json object

--- src/main/java/forms/application/views/PreInspectionView.java
+++ src/main/java/forms/application/views/PreInspectionView.java
@@ -1,8 +1,10 @@
 package forms.application.views;
 
+import com.nimbusds.jose.shaded.gson.JsonObject;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.shared.Tooltip;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
@@ -17,11 +19,21 @@ import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import org.springframework.stereotype.Service;
 
 
 @PageTitle("PreInspection")
 @Route(value = "PreInspection")
+@Service
 public class PreInspectionView extends VerticalLayout {
+    private TextArea petition = new TextArea();
+    private Checkbox preparation = new Checkbox();
+    private Checkbox safety = new Checkbox();
+    private TextArea codes = new TextArea();
+    private TextArea colorGaze = new TextArea();
+    private TextArea noise = new TextArea();
+    private TextArea checkMotion = new TextArea();
+
     public PreInspectionView() {
         setPreInspectionView();
     }
@@ -30,10 +42,12 @@ public class PreInspectionView extends VerticalLayout {
         H1 h1 = new H1("ОСМОТР ТЕХНИКИ");
         H2 h2 = new H2("Предварительный осмотр");
         Button proceed = new Button("Продолжить");
-        proceed.addClickListener(e ->
+        proceed.addClickListener(e ->{
+            if (checkNull()) {
                 proceed.getUI().ifPresent(ui ->
-                        ui.navigate("UpperLevel")));
-
+                        ui.navigate("UpperLevel"));
+            }
+        });
         Button back = new Button("Назад");
         back.addClickListener(e ->
                 back.getUI().ifPresent(ui ->
@@ -42,19 +56,19 @@ public class PreInspectionView extends VerticalLayout {
         add(h1);
         add(h2);
 
-        getPhoto();
-        getPetition();
-        getPreparation();
-        getSafety();
-        getCode();
-        getColorGaze();
-        getNoise();
+        photo();
+        petition();
+        preparation();
+        safety();
+        errorСodes();
+        сolorGaze();
+        noise();
         checkMotion();
 
         add(new HorizontalLayout(back, proceed));
     }
 
-    private void getPhoto() {
+    private void photo() {
         var buffer = new MultiFileMemoryBuffer();
         Button buttonPhoto = new Button("Загрузить фото");
         Upload setPhoto = new Upload(buffer);
@@ -68,14 +82,14 @@ public class PreInspectionView extends VerticalLayout {
         add(getPhoto);
     }
 
-    private void getPetition() {
+    private void petition() {
         String label = "Жалобы, предложения";
-        String textHelper = "Запишите жалобы, пожелания оператора";
-        TextArea petition = setTextArea(label, textHelper);
+        String textHelper = "Запишите жалобы, пожелания оператора. При их отсутствии напишите \"нет\"";
+        petition = setTextArea(label, textHelper);
         add(petition);
     }
 
-    private void getPreparation() {
+    private void preparation() {
         String label = "Подготовка к проверке";
         String textHelper = """
                 Припаркуйте машину на ровном месте, свободном от людей и препятствий.
@@ -83,54 +97,54 @@ public class PreInspectionView extends VerticalLayout {
                 Включите стояночный тормоз
                 Опустите рабочие инструменты на землю
                 Поверните переключатель запуска двигателя в положение «ВЫКЛ» и выньте ключ.""";
-        Checkbox preparation = setCheckbox(label, textHelper);
+        preparation = setCheckbox(label, textHelper);
         add(preparation);
     }
 
-    private void getSafety(){
+    private void safety() {
         String label = "Проверка безопасности";
         String textHelper = """
                 Убедитесь, что под машиной, вокруг машины или на ней нет никого; убедиться, что в близи нет персонала
                 Обойдите машину и осмотрите нижнюю часть машины на наличие видимых признаков утечек жидкости.
                 Проверьте уровень моторного масла, гидравлического масла, трансмиссионного масла и охлаждающей жидкости; при необходимости долейте масло или охлаждающую жидкость до минимального уровня для каждой системы.""";
-        Checkbox safety = setCheckbox(label, textHelper);
+        safety = setCheckbox(label, textHelper);
         add(safety);
     }
 
-    private void  getCode(){
+    private void errorСodes() {
         String label = "Коды неисправностей";
         String textHelper = "Запишите  неисправности машины из журнала ошибок" +
                 " на панели приборов";
-        TextArea codes = setTextArea(label, textHelper);
+        codes = setTextArea(label, textHelper);
         add(codes);
     }
 
-    private void getColorGaze(){
+    private void сolorGaze() {
         String label = "Цвет выхлопных газов двигателя";
         String textHelper = "Запустите машину и пронаблюдайте за цветом выхлопных газов.\n" +
                 "Запишите цвет выхлопных газов";
-        TextArea colorGaze = setTextArea(label, textHelper);
+        colorGaze = setTextArea(label, textHelper);
         add(colorGaze);
     }
 
-    private  void getNoise(){
+    private void noise() {
         String label = "Необычные шумы";
         String textHelper = """
                 Повышенный шум, исходящий от гидравлического насоса
                 Повышенный шум из моторного отсека
                 Повышенный шум, исходящий от передних и задних дифференциалов/конечных передач и трансмиссии""";
-        TextArea noise = setTextArea(label, textHelper);
+        noise = setTextArea(label, textHelper);
         add(noise);
     }
 
-    private void checkMotion(){
+    private void checkMotion() {
         String label = "Проверка в движении";
         String textHelper = "Удары, толчки при переключении направлений хода, передач, выполнении поворотов";
-        TextArea checkMotion = setTextArea(label, textHelper);
+        checkMotion = setTextArea(label, textHelper);
         add(checkMotion);
     }
 
-    private TextArea setTextArea(String label, String textHelper){
+    private TextArea setTextArea(String label, String textHelper) {
         TextArea textArea = new TextArea(label);
         Button button = new Button(new Icon(VaadinIcon.INFO_CIRCLE));
         button.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE,
@@ -142,7 +156,7 @@ public class PreInspectionView extends VerticalLayout {
         return textArea;
     }
 
-    private Checkbox setCheckbox(String label, String textHelper){
+    private Checkbox setCheckbox(String label, String textHelper) {
         Checkbox checkbox = new Checkbox(label);
         Button button = new Button(new Icon(VaadinIcon.INFO_CIRCLE));
         button.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE,
@@ -152,6 +166,61 @@ public class PreInspectionView extends VerticalLayout {
         Tooltip tooltip = checkbox.getTooltip().withManual(true);
         button.addClickListener(event -> tooltip.setOpened(!tooltip.isOpened()));
         return checkbox;
+    }
+
+    public JsonObject getPreInspectionViewJsonObject() {
+        JsonObject jsonObject = new JsonObject();
+        JsonObject preInspection = new JsonObject();
+        preInspection.addProperty("petition", petition.getValue());
+        preInspection.addProperty("preparation", preparation.getValue());
+        preInspection.addProperty("safety", safety.getValue());
+        preInspection.addProperty("codes", codes.getValue());
+        preInspection.addProperty("colorGaze", colorGaze.getValue());
+        preInspection.addProperty("noise", noise.getValue());
+        preInspection.addProperty("checkMotion", checkMotion.getValue());
+        jsonObject.add("preInspection", preInspection);
+        return jsonObject;
+    }
+
+    private boolean checkNull() {
+        if (petition.isEmpty()) {
+            Notification.show("Заполните жалобы. предложения");
+            petition.focus();
+            return false;
+        }
+        if (preparation.isEmpty()) {
+            Notification.show("Подготовьте технику к проверке");
+            preparation.focus();
+            return false;
+        }
+
+        if (safety.isEmpty()) {
+            Notification.show("Выполните меры безопасности");
+            safety.focus();
+            return false;
+        }
+
+        if (codes.isEmpty()) {
+            Notification.show("Заполните коды неисправностей");
+            codes.focus();
+            return false;
+        }
+        if (colorGaze.isEmpty()) {
+            Notification.show("Заполните цвет выхлопных газов");
+            colorGaze.focus();
+            return false;
+        }
+        if (noise.isEmpty()) {
+            Notification.show("Заполните необычные шумы");
+            noise.focus();
+            return false;
+        }
+        if (checkMotion.isEmpty()) {
+            Notification.show("Заполните проверку в движении");
+            checkMotion.focus();
+            return false;
+        }
+        return true;
     }
 
 }
