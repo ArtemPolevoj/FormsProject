49c516faad06a9e38c8f84350c49df74ba0dbd0c
artem <artempolevoj@yandex.ru>
2024-09-29 19:35:49 +0300

    add getDefect and getListWork

--- src/main/java/forms/application/views/QuestionView.java
+++ src/main/java/forms/application/views/QuestionView.java
@@ -3,13 +3,16 @@ package forms.application.views;
 
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.NativeLabel;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.shared.Tooltip;
 import com.vaadin.flow.component.textfield.TextArea;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.component.upload.receivers.MultiFileMemoryBuffer;
 
@@ -19,6 +22,8 @@ public class QuestionView extends Composite<VerticalLayout> {
     private final String number;
     private final String text;
     private final String explanations;
+    private TextArea defect;
+    private TextArea listWork;
 
     public QuestionView(String number, String text, String explanations) {
         this.number = number;
@@ -28,13 +33,9 @@ public class QuestionView extends Composite<VerticalLayout> {
     }
 
     private void setQuestion() {
-        TextField numberQuestion = new TextField("Номер вопроса");
-        numberQuestion.setValue(number);
-        numberQuestion.setReadOnly(true);
-        TextArea textQuestion = new TextArea("Вопрос");
+        TextArea textQuestion = setTextArea(number, explanations);
         textQuestion.setValue(text);
         textQuestion.setReadOnly(true);
-        Button buttonExplanations = new Button("?");
         Button buttonNoComments = new Button("Нет замечаний");
         //<theme-editor-local-classname>
         buttonNoComments.addClassName("question-view-button-1");
@@ -42,14 +43,10 @@ public class QuestionView extends Composite<VerticalLayout> {
         //<theme-editor-local-classname>
         buttonYesComments.addClassName("question-view-button-2");
 
-        TextArea defect = new TextArea("Неисправность");
-        Button buttonDefectShow = new Button("?");
+        defect = setTextArea("Неисправность", "Опишите неисправность");
 
-        buttonDefectShow.addClickListener(e -> Notification.show("Опишите неисправность"));
-        TextArea listWork = new TextArea("Список необходимых з.ч. и/или работ");
-        Button buttonListWorkShow = new Button("?");
-        buttonListWorkShow.addClickListener(e -> Notification.show("Добавьте необходимые з.ч. или опишите их\n" +
-                "Добавьте необходимые работы"));
+        listWork = setTextArea("Список необходимых з.ч. и/или работ",
+                "Добавьте необходимые з.ч. или опишите их. Добавьте необходимые работы");
         var buffer = new MultiFileMemoryBuffer();
         Button buttonPhoto = new Button("Загрузить фото");
 
@@ -64,9 +61,9 @@ public class QuestionView extends Composite<VerticalLayout> {
 
         Div getPhoto = new Div(photoLabel, setPhoto);
 
-        HorizontalLayout defectLayout = new HorizontalLayout(defect, buttonDefectShow);
+        HorizontalLayout defectLayout = new HorizontalLayout(defect);
 
-        HorizontalLayout listWorkLayout = new HorizontalLayout(listWork, buttonListWorkShow);
+        HorizontalLayout listWorkLayout = new HorizontalLayout(listWork);
 
 VerticalLayout mainDefect = new VerticalLayout(defectLayout, listWorkLayout, getPhoto);
 mainDefect.setVisible(false);
@@ -82,18 +79,34 @@ mainDefect.setVisible(false);
         buttonAbsent.addClickListener(e -> mainDefect.setVisible(false) );
         //<theme-editor-local-classname>
         buttonAbsent.addClassName("question-view-button-3");
-        buttonExplanations.addClickListener(e -> Notification.show(explanations));
+
         buttonNoComments.addClickListener(e -> mainDefect.setVisible(false) );
 
-        HorizontalLayout texts = new HorizontalLayout(textQuestion, buttonExplanations);
+        HorizontalLayout texts = new HorizontalLayout(textQuestion);
         HorizontalLayout firstLineButton = new HorizontalLayout(buttonNoComments, buttonYesComments);
         HorizontalLayout secondLineButton = new HorizontalLayout(buttonNoInspection, buttonAbsent);
-        getContent().add(numberQuestion);
         getContent().add(texts);
         getContent().add(firstLineButton);
         getContent().add(secondLineButton);
 
 
         getContent().add(mainDefect);
+    }
+    private TextArea setTextArea(String label, String textHelper) {
+        TextArea textArea = new TextArea(label);
+        Button button = new Button(new Icon(VaadinIcon.INFO_CIRCLE));
+        button.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE,
+                ButtonVariant.LUMO_ICON);
+        textArea.setSuffixComponent(button);
+        textArea.setTooltipText(textHelper);
+        Tooltip tooltip = textArea.getTooltip().withManual(true);
+        button.addClickListener(event -> tooltip.setOpened(!tooltip.isOpened()));
+        return textArea;
+    }
+    public String getDefect(){
+        return defect.getValue();
+    }
+    public String getListWork(){
+        return listWork.getValue();
     }
 }
