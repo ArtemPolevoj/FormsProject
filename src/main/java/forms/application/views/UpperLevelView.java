a14c97b09e149ddd577e3deebb075f22b87d1456
artem <artempolevoj@yandex.ru>
2024-11-09 16:11:31 +0300

    update UpperLevelView for get json

--- src/main/java/forms/application/views/UpperLevelView.java
+++ src/main/java/forms/application/views/UpperLevelView.java
@@ -1,38 +1,54 @@
 package forms.application.views;
 
+import com.nimbusds.jose.shaded.gson.JsonObject;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import forms.application.model.QuestionEntity;
+import forms.application.service.ImageServiceImpl;
+import forms.application.service.QuestionServiceImpl;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @PageTitle("UpperLevel")
 @Route(value = "UpperLevel")
 @Service
 public class UpperLevelView extends VerticalLayout {
-    private final InputDataView dataView;
+    private final JsonObject upperLevel = new JsonObject();
+    private final List<QuestionEntity> questions;
+    private final List<QuestionView> questionViews = new ArrayList<>();
+    private final ImageServiceImpl imageService;
+    private boolean flag = true;
 
 
-    public UpperLevelView(InputDataView dataView) {
-        this.dataView = dataView;
-
-
+    public UpperLevelView(QuestionServiceImpl questionService, ImageServiceImpl imageService) {
+        this.imageService = imageService;
+        questions = questionService.findAll();
         setUpperLevelView();
     }
-    private void setUpperLevelView(){
+
+    private void setUpperLevelView() {
         H1 h1 = new H1("ОСМОТР ТЕХНИКИ");
         H2 h2 = new H2("Осмотр верхнего уровня");
         Button proceed = new Button("Продолжить");
-        proceed.addClickListener(e ->
+        proceed.addClickListener(e -> {
+            getValue();
+            if (flag){
                 proceed.getUI().ifPresent(ui ->
-                        ui.navigate("AverageLevel")));
+                        ui.navigate("AverageLevel"));
+            }else {
+                getValue();
+            }
+
+        });
+
 
         Button back = new Button("Назад");
         back.addClickListener(e -> getUI().ifPresent(ui ->
@@ -40,18 +56,32 @@ public class UpperLevelView extends VerticalLayout {
 
         add(h1);
         add(h2);
-        //генерим серию вопросов
-        Paragraph paragraph = new Paragraph();
-        paragraph.setText("Здесь генерим серию вопросов");
-        QuestionView question = new QuestionView("1", "Вопрос", "Пояснения");
 
-        add(paragraph);
-        add(question);
+        for (int i = 0; i < 5; i++) {
+            String number = String.valueOf(i + 1);
+            String text = questions.get(i).getDescription();
+            QuestionView question = new QuestionView(imageService, number, text, "Пояснения");
+            questionViews.add(question);
+            add(question);
+        }
 
         add(new HorizontalLayout(back, proceed));
 
-
     }
 
+    public JsonObject getUpperLevelJsonObject() {
+        return upperLevel;
+    }
+
+    private void getValue() {
+        for (int i = 0; i < questionViews.size(); i++) {
+            QuestionView questionView = questionViews.get(i);
+            flag = questionView.checkNull();
+            if (!flag){
+                break;
+            }
+            upperLevel.add(String.valueOf(i + 1), questionView.getQuestion());
+        }
+    }
 
 }
